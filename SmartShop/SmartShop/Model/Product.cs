be64aa1f6a2640f74c82d4fb2e6b992c0e61aea1
prizo <prizo1995@gmail.com>
2018-03-11 01:43:38 -0600

    Implemented search bar

---
+++ SmartShop/SmartShop/Model/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartShop.Model
+{
+    class Product
+    {
+        public string DataUrl { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Offer { get; set; }
+        public List<Vendor> OtherOptions { get; set; }
+        public string Details { get; set; }
+    }
+}
