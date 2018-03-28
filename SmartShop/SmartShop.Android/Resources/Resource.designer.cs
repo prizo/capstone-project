e7e1d552829dd34ac5888fde9449af9d4c6f695c
prizo <prizo1995@gmail.com>
2018-03-28 00:29:34 -0500

    Added new icon and splash to Android

--- SmartShop/SmartShop.Android/Resources/Resource.designer.cs
+++ SmartShop/SmartShop.Android/Resources/Resource.designer.cs
@@ -2198,26 +2198,26 @@ namespace SmartShop.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020112
-			public const int avd_hide_password_1 = 2130837778;
-			
-			// aapt resource value: 0x7f020113
-			public const int avd_hide_password_2 = 2130837779;
-			
 			// aapt resource value: 0x7f020114
-			public const int avd_hide_password_3 = 2130837780;
+			public const int avd_hide_password_1 = 2130837780;
+			
+			// aapt resource value: 0x7f020115
+			public const int avd_hide_password_2 = 2130837781;
+			
+			// aapt resource value: 0x7f020116
+			public const int avd_hide_password_3 = 2130837782;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020115
-			public const int avd_show_password_1 = 2130837781;
-			
-			// aapt resource value: 0x7f020116
-			public const int avd_show_password_2 = 2130837782;
-			
 			// aapt resource value: 0x7f020117
-			public const int avd_show_password_3 = 2130837783;
+			public const int avd_show_password_1 = 2130837783;
+			
+			// aapt resource value: 0x7f020118
+			public const int avd_show_password_2 = 2130837784;
+			
+			// aapt resource value: 0x7f020119
+			public const int avd_show_password_3 = 2130837785;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2774,17 +2774,23 @@ namespace SmartShop.Droid
 			// aapt resource value: 0x7f02010d
 			public const int notification_icon_background = 2130837773;
 			
-			// aapt resource value: 0x7f020110
-			public const int notification_template_icon_bg = 2130837776;
+			// aapt resource value: 0x7f020112
+			public const int notification_template_icon_bg = 2130837778;
 			
-			// aapt resource value: 0x7f020111
-			public const int notification_template_icon_low_bg = 2130837777;
+			// aapt resource value: 0x7f020113
+			public const int notification_template_icon_low_bg = 2130837779;
 			
 			// aapt resource value: 0x7f02010e
 			public const int notification_tile_bg = 2130837774;
 			
 			// aapt resource value: 0x7f02010f
 			public const int notify_panel_notification_icon_bg = 2130837775;
+			
+			// aapt resource value: 0x7f020110
+			public const int splash = 2130837776;
+			
+			// aapt resource value: 0x7f020111
+			public const int splash_screen = 2130837777;
 			
 			static Drawable()
 			{
@@ -4373,6 +4379,9 @@ namespace SmartShop.Droid
 			
 			// aapt resource value: 0x7f0c018a
 			public const int MainTheme_Base = 2131493258;
+			
+			// aapt resource value: 0x7f0c018c
+			public const int MyTheme_Splash = 2131493260;
 			
 			// aapt resource value: 0x7f0c002d
 			public const int Platform_AppCompat = 2131492909;
