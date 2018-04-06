b592ce4e6ba7e05479657aaf5689b94cdf0ffd74
prizo <prizo1995@gmail.com>
2018-04-05 23:20:09 -0500

    Added higher res splash img

--- SmartShop/SmartShop/View/ProductPage.xaml.cs
+++ SmartShop/SmartShop/View/ProductPage.xaml.cs
@@ -1,9 +1,7 @@
-﻿using Plugin.Geolocator;
-using SmartShop.Model;
+﻿using SmartShop.Model;
 using SmartShop.ViewModel;
-using System.Linq;
+using System.Collections.Generic;
 using Xamarin.Forms;
-using Xamarin.Forms.GoogleMaps;
 using Xamarin.Forms.Xaml;
 
 namespace SmartShop.View
@@ -11,10 +9,29 @@ namespace SmartShop.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ProductPage : ContentPage
 	{
+        List<string> sellers = new List<string>()
+        {
+            "Ace Hardware", "Apple Store", "Best Buy", "Boost Mobile", "Dollar General",
+            "DICK'S Sporting Goods", "GameStop", "GNC", "JCPenney", "Kmart", "Kohl's", "Lowe's",
+            "Macy's", "Nordstrom", "Office Depot", "OfficeMax", "Sam's Club", "Sears", "Sprint",
+            "Staples", "Target", "T-Mobile", "Ulta", "Verizon Wireless", "Walgreens", "Walmart",
+            "Wal-Mart.com"
+        };
+
         public ProductPage (Product product, bool isEnabled)
 		{
 			InitializeComponent ();
             BindingContext = new ProductPageViewModel(product, isEnabled);
+
+            if (!sellers.Contains(product.Seller))
+            {
+                map.IsEnabled = false;
+                map.IsVisible = false;
+            }
+            else
+            {
+                // Add a pin to the map
+            }
         }
     }
 }
