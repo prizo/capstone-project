102df272d141ad88027f6f5f8b6da49257b8a230
prizo <prizo1995@gmail.com>
2018-04-08 16:15:53 -0500

    Set screen orientation to portrait only

--- SmartShop/SmartShop.Android/MainActivity.cs
+++ SmartShop/SmartShop.Android/MainActivity.cs
@@ -11,7 +11,8 @@ using Plugin.Permissions.Abstractions;
 
 namespace SmartShop.Droid
 {
-    [Activity(Label = "SmartShop", Icon = "@mipmap/ic_launcher", RoundIcon = "@mipmap/ic_round_launcher", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "SmartShop", Icon = "@mipmap/ic_launcher", RoundIcon = "@mipmap/ic_round_launcher", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
