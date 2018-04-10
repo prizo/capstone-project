461d4e7db73e6612f204f75a87442937d68f1663
prizo <prizo1995@gmail.com>
2018-04-09 23:31:50 -0500

    Finished integrating Vision API

--- SmartShop/SmartShop/ViewModel/CameraPageViewModel.cs
+++ SmartShop/SmartShop/ViewModel/CameraPageViewModel.cs
@@ -2,21 +2,73 @@
 using Google.Apis.Vision.v1;
 using Google.Apis.Vision.v1.Data;
 using Plugin.Media;
+using Plugin.Media.Abstractions;
+using SmartShop.Model;
+using SmartShop.Utilities;
+using SmartShop.View;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace SmartShop.ViewModel
 {
-    class CameraPageViewModel
+    class CameraPageViewModel : INotifyPropertyChanged
     {
         public CameraPageViewModel()
         {
             TakePhotoCommand = new Command(HandleTakePhoto);
             SelectPhotoCommand = new Command(HandleSelectPhoto);
+        }
+
+        private Xamarin.Forms.ImageSource _source = "placeholder.png";
+
+        public Xamarin.Forms.ImageSource Source
+        {
+            get
+            {
+                return _source;
+            }
+            set
+            {
+                _source = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isRunning = false;
+
+        public bool IsRunning
+        {
+            get
+            {
+                return _isRunning;
+            }
+            set
+            {
+                _isRunning = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isVisible = false;
+
+        public bool IsVisible
+        {
+            get
+            {
+                return _isVisible;
+            }
+            set
+            {
+                _isVisible = value;
+                OnPropertyChanged();
+            }
         }
 
         public ICommand TakePhotoCommand { get; private set; }
@@ -31,16 +83,13 @@ namespace SmartShop.ViewModel
                 return;
             }
 
-            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
             {
                 Directory = "Vision",
                 Name = "photo.jpg"
             });
 
-            if (file == null)
-                return;
-
-            AnnotateAsync(file.Path);
+            ProcessImage(file);
         }
 
         public ICommand SelectPhotoCommand { get; private set; }
@@ -57,13 +106,68 @@ namespace SmartShop.ViewModel
 
             var file = await CrossMedia.Current.PickPhotoAsync();
 
+            ProcessImage(file);
+        }
+
+        private async void ProcessImage(MediaFile file)
+        {
             if (file == null)
                 return;
 
-            AnnotateAsync(file.Path);
+            // Show photo and enable loading animation
+            Source = file.Path;
+            IsRunning = true;
+            IsVisible = true;
+
+            // Annotate image on another thread
+            string keywords = "";
+            try
+            {
+                await Task.Run(async () =>
+                {
+                    keywords = await AnnotateAsync(file.Path);
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            // Show placeholder and disable loading animation
+            Source = "placeholder.png";
+            IsRunning = false;
+            IsVisible = false;
+
+            // Search for the item using Bing shop
+            if (keywords.Trim() != "")
+            {
+                string document = "";
+                IList<Product> products = null;
+
+                string query = Uri.EscapeDataString(keywords.Trim());
+                document = BingWebRequest.SendRequest("/shop?q=" + query);
+
+                if (document != null && document != "")
+                {
+                    products = new ProductExtractor().ExtractProducts(document);
+                }
+
+                if (products != null && products.Count > 0)
+                {
+                    await Application.Current.MainPage.Navigation.PushModalAsync(new NavigationPage(new ResultPage(products)));
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("", "No results", "OK");
+                }
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("", "No results", "OK");
+            }
         }
 
-        private async void AnnotateAsync(string path)
+        private async Task<string> AnnotateAsync(string path)
         {
             // Create the service
             var service = new VisionService(new BaseClientService.Initializer
@@ -74,7 +178,7 @@ namespace SmartShop.ViewModel
             var bytes = File.ReadAllBytes(path);
 
             // Create the image request
-            var imageRequest = new AnnotateImageRequest
+            var imgReq = new AnnotateImageRequest
             {
                 Image = new Google.Apis.Vision.v1.Data.Image
                 {
@@ -92,23 +196,36 @@ namespace SmartShop.ViewModel
             {
                 Requests = new List<AnnotateImageRequest>
                 {
-                    imageRequest
+                    imgReq
                 }
             };
 
             // Get the response
             var result = await service.Images.Annotate(request).ExecuteAsync();
 
-            if (result?.Responses?.Count > 0)
+            // Extract the keywords
+            string keywords = "";
+
+            if (result?.Responses?.Count > 0 && result.Responses[0].TextAnnotations != null)
             {
-                var keywords = result.Responses[0].TextAnnotations.Select(s => s.Description).ToArray();
-                var words = String.Join(",", keywords);
-                await Application.Current.MainPage.DisplayAlert("Result", words, "OK");
+                var desc = result.Responses[0].TextAnnotations[0].Description;
+
+                string[] words = desc.Split(
+                    new[] { "\r\n", "\r", "\n" },
+                    StringSplitOptions.None
+                );
+
+                keywords = String.Join(" ", words);
             }
-            else
-            {
-                await Application.Current.MainPage.DisplayAlert("Result", "Nothing found", "OK");
-            }
+
+            return keywords;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string caller = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
         }
     }
 }
